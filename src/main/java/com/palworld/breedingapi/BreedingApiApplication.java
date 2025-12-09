75017918987aabe695af184935a63967a2d5f0b7
UserOtt Pessoal <labussework@gmail.com>
2025-12-09 18:27:04 -0300

    fix: rename package

--- src/main/java/com/palworld/breedingapi/BreedingApiApplication.java
+++ src/main/java/com/palworld/breedingapi/BreedingApiApplication.java
@@ -1,4 +1,4 @@
-package com.palworld.breeding_api;
+package com.palworld.breedingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
